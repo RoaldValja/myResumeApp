55f0573b62be06f1559c3a9bedec1c950a5d7219
Roald Välja <roaldvlja@gmail.com>
2022-02-26 07:44:23 +0200

    MongoDB addition

--- models/db.js
+++ models/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-
+/*
 mongoose.connect('mongodb://localhost:27017/resumeAppDB',
 { useNewUrlParser: true, useUnifiedTopology: true});
-/*
+*/
+
 mongoose.connect(process.env.MONGO_URI,
     { useNewUrlParser: true, useUnifiedTopology: true});
 
-    */
+    
 require('./user');
 require('./backendSkill');
 require('./frontendSkill');
